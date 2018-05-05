bf9b21eb51469194351c4e888e5c2d06dce4742a
Antonio DiNostri <semiviral@gmail.com>
2018-05-05 08:39:08 -0500

    added GetMessagesByDateTimeOrDefault() logic

--- Convex.Net/Convex.Net/Controllers/MessagesController.cs
+++ Convex.Net/Convex.Net/Controllers/MessagesController.cs
@@ -5,31 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Convex.Net.Controllers {
     [Route("api/[controller]")]
     public class MessagesController : Controller {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get() {
-            return new[] {"value1", "value2"};
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id) {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value) { }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value) { }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id) { }
-
-
+        //GET api/Messages
         [HttpGet("{datetime}")]
         public void Get(string lastMessageReceivedDateTime) {
             // todo get logic
--- Convex.Net/Convex.Net/Models/Server.cs
+++ Convex.Net/Convex.Net/Models/Server.cs
@@ -1,29 +1,75 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Convex.IRC;
+using Convex.IRC.ComponentModel.Event;
+using Convex.IRC.Model;
 
 namespace Convex.Net.Models {
     public class Server {
         #region MEMBERS
 
-        private Client client { get; }
+        public Client Client { get; }
 
         public IPEndPoint IpAddress { get; }
+
+        private List<ServerMessage> Messages { get; }
 
         #endregion
 
         public Server(IPEndPoint ipAddress) {
             IpAddress = ipAddress;
 
-            client = new Client(IpAddress);
+            Client = new Client(IpAddress);
+            Client.Server.ChannelMessaged += OnClientChannelMessaged;
+
+            Messages = new List<ServerMessage>();
         }
+
+        #region EVENT
+
+        private Task OnClientChannelMessaged(object sender, ServerMessagedEventArgs args) {
+            Messages.Add(args.Message);
+
+            return Task.CompletedTask;
+        }
+
+        #endregion
 
         #region INIT
 
         public async Task Initialise() {
-            await client.Initialise();
+            await Client.Initialise();
         }
 
         #endregion
+
+        #region METHODS
+
+        public IEnumerable<ServerMessage> GetMessagesByDateTimeOrDefault(DateTime referenceTime, DateTimeOrdinal dateTimeOrdinal) {
+            IEnumerable<ServerMessage> temporaryList = null;
+
+            switch (dateTimeOrdinal) {
+                case DateTimeOrdinal.Before:
+                    temporaryList = Messages.Where(message => message.Timestamp > referenceTime);
+                    break;
+                case DateTimeOrdinal.After:
+                    temporaryList = Messages.Where(message => message.Timestamp < referenceTime);
+                    break;
+                default:
+                    break;
+            }
+
+            return temporaryList;
+        }
+
+        #endregion
+    }
+
+    public enum DateTimeOrdinal {
+        Before,
+        After
     }
 }
