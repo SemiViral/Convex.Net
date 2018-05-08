5d8142393989fa99b250e3c4f78b5964f434a819
Antonio DiNostri <semiviral@gmail.com>
2018-05-08 09:56:48 -0500

    Get(DateTime) to Get(string)
    Get(string) DateTime format is YYYYMMDDhhmmss

--- Convex.Net/Convex.Net/Controllers/IrcController.cs
+++ Convex.Net/Convex.Net/Controllers/IrcController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using Convex.IRC.Model;
 using Convex.Net.Model;
@@ -20,9 +21,9 @@ namespace Convex.Net.Controllers {
 
         //GET api/irc
         [HttpGet("{dateTime}")]
-        public List<ServerMessage> Get(DateTime dateTime) {
-            // DateTime.MinValue == 1/1/0001 12:00:00 AM
-            return IrcClientReference.GetMessagesByDateTimeOrDefault(dateTime, DateTimeOrdinal.After).ToList();
+        public List<ServerMessage> Get(string dateTime) {
+            // DateTime.MinValue == 1/1/0001 12:00:00 AM (YYYYMMDDhhmmss 00010101120000)
+            return IrcClientReference.GetMessagesByDateTimeOrDefault(DateTime.ParseExact(dateTime, "YYYYMMDDhhmmss", CultureInfo.InvariantCulture), DateTimeOrdinal.After).ToList();
         }
     }
 }
