5b713d273a9cd2a11ee108b56e22441c862f49e1
Antonio DiNostri <semiviral@gmail.com>
2018-05-13 20:42:27 -0500

    SecurityCapsule to Passphrase

---
+++ Convex.Net/Convex.Net/Model/Passphrase.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Convex.Net.Model {
+    public class SecurityCapsule {
+        #region MEMBERS
+
+        private string Hash { get; }
+
+        #endregion
+
+
+        public SecurityCapsule(string passphrase) {
+            using (SHA512 hashManager = SHA512.Create()) {
+                Hash = hashManager.ComputeHash(Encoding.UTF8.GetBytes(passphrase));
+            }
+        }
+    }
+}
