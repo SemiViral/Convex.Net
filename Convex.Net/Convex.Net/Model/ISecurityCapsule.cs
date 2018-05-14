1f7869a033dd01e589bf5a189f8e9b8a33a01bc6
Antonio DiNostri <semiviral@gmail.com>
2018-05-13 20:33:22 -0500

    added ISecurityCapsule members

--- Convex.Net/Convex.Net/Model/ISecurityCapsule.cs
+++ Convex.Net/Convex.Net/Model/ISecurityCapsule.cs
@@ -2,6 +2,13 @@
 
 namespace Convex.Net.Model {
     public interface ISecurityCapsule {
+        #region MEMBERS
 
+        SHA512 Hash { get; }
+        bool IsInitialised { get; set; }
+
+        #endregion
+
+        void Initialise(string passphrase);
     }
 }
