115f53d1c82ccfdc90ea589ea6017aa38b4b8a73
Antonio DiNostri <semiviral@gmail.com>
2018-05-13 20:49:03 -0500

    added Passphrase to ClientController

--- Convex.Net/Convex.Net/Controllers/ClientController.cs
+++ Convex.Net/Convex.Net/Controllers/ClientController.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Convex.Net.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Convex.Net.Controllers {
     [Route("api/[controller]")]
     public class ClientController {
+        #region MEMBERS
+
+        private Passphrase Passphrase { get; }
+
+        #endregion
+
+        public ClientController(Passphrase passphrase) {
+            Passphrase = passphrase;
+        }
+
         [HttpGet("{isAlive}")]
         public bool Get() {
             return true;
         }
 
-        [HttpPost("{passphrase}")]
-        public void Post(string passphrase) {
-
-        }
+        [HttpGet("{passphrase}")]
+        public void Get(string passphrase) { }
     }
 }
