55a279f3ed2d024a44eaf24c0e780e1129f008b2
Antonio DiNostri <semiviral@gmail.com>
2018-05-13 20:30:15 -0500

    added Initialisation for SecurityCapsule

--- Convex.Net/Convex.Net/Model/SecurityCapsule.cs
+++ Convex.Net/Convex.Net/Model/SecurityCapsule.cs
@@ -1,3 +1,9 @@
 ﻿namespace Convex.Net.Model {
-    public static class SecurityCapsule { }
+    public static class SecurityCapsule {
+        public static bool IsInitialised { get; private set; }
+
+        public static void Initialise(string passphrase) {
+
+        }
+    }
 }
