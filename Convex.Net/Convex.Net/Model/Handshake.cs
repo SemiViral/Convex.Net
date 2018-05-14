c1bb42e3e9ebc4dd4a97cc284709ee0e228b08c4
Antonio DiNostri <semiviral@gmail.com>
2018-05-14 07:46:51 -0500

    Hash to PrivateKey

--- Convex.Net/Convex.Net/Model/Handshake.cs
+++ Convex.Net/Convex.Net/Model/Handshake.cs
@@ -8,19 +8,17 @@ namespace Convex.Net.Model {
         #region MEMBERS
 
         public KeyValuePair<int, byte[]> PublicKey { get;}
-        private byte[] Hash { get; }
+        private byte[] PrivateKey { get; }
 
         #endregion
 
 
         public Handshake() {
-            using (SHA512 hashManager = SHA512.Create()) {
-                Hash = hashManager.ComputeHash(Encoding.UTF8.GetBytes(passphrase));
-            }
+            
         }
 
         public bool Verify(string passphrase) {
-            return Hash.SequenceEqual(Encoding.UTF8.GetBytes(passphrase));
+            return PrivateKey.SequenceEqual(Encoding.UTF8.GetBytes(passphrase));
         }
     }
 }
